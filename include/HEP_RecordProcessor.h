2eff4517dc4bebf38b23865535d2e39dfbb141c4
Joergen Samson <joergen.samson@desy.de>
2006-03-31 21:51:47 +0000

    Fix of syntax for compatibilty with gcc4

--- include/HEP_RecordProcessor.h
+++ include/HEP_RecordProcessor.h
@@ -8,32 +8,34 @@ using namespace lcio ;
 using namespace marlin ;
 
 
-/**  Displays the MCParticles in CED. <br>
- *  Color scheme:<br>
- *  neutral hadrons:  pink 'clusters' + magenta 'tracks' <br>
- *  photons: yellow 'clusters' <br>
- *  charged tracks:  green <br>
- *  electrons:  magenta-pink  <br>        
- *  muons:  light green <br>
- * <p> Toggle display of neutrals with [1] and charged Tracks with [Shift-1].
- * <p> F.Gaede: changed to use MarlinCED
- * 
- * @author V.Morgunov, DESY
- * @version $Id: HEP_RecordProcessor.h,v 1.1.1.1 2005-08-04 08:58:35 gaede Exp $
- */
-class HEP_RecordProcessor : public Processor {
+namespace marlin {
+  /**  Displays the MCParticles in CED. <br>
+   *  Color scheme:<br>
+   *  neutral hadrons:  pink 'clusters' + magenta 'tracks' <br>
+   *  photons: yellow 'clusters' <br>
+   *  charged tracks:  green <br>
+   *  electrons:  magenta-pink  <br>        
+   *  muons:  light green <br>
+   * <p> Toggle display of neutrals with [1] and charged Tracks with [Shift-1].
+   * <p> F.Gaede: changed to use MarlinCED
+   * 
+   * @author V.Morgunov, DESY
+   * @version $Id: HEP_RecordProcessor.h,v 1.2 2006-03-31 21:51:47 samson Exp $
+   */
+  class HEP_RecordProcessor : public Processor {
 
- public:
-  virtual Processor*  newProcessor() { return new HEP_RecordProcessor ; }
-  HEP_RecordProcessor() ;
-  virtual void init() ; //the begin of the job
-  virtual void processRunHeader( LCRunHeader* run ) ; //  every run.
-  virtual void processEvent( LCEvent * evt ) ;   // very event
-  virtual void check( LCEvent * evt ) ; 
-  virtual void end() ; //  after data processing 
-protected:
-  int _nRun ;
-  int _nEvt ;
-} ;
+  public:
+    virtual Processor*  newProcessor() { return new HEP_RecordProcessor ; }
+    HEP_RecordProcessor() ;
+    virtual void init() ; //the begin of the job
+    virtual void processRunHeader( LCRunHeader* run ) ; //  every run.
+    virtual void processEvent( LCEvent * evt ) ;   // very event
+    virtual void check( LCEvent * evt ) ; 
+    virtual void end() ; //  after data processing 
+  protected:
+    int _nRun ;
+    int _nEvt ;
+  } ;
 
+}// namespace marlin
 #endif
