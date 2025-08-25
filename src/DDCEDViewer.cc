9682000f87de44a09680cc9fc95e32843b10ca7a
Stefano Franchellucci <stefano.franchellucci@cern.ch>
2025-08-25 17:18:07 +0200

    fix typo in comment

--- src/DDCEDViewer.cc
+++ src/DDCEDViewer.cc
@@ -1221,7 +1221,7 @@ CalorimeterDrawParams getCalorimeterParameters(dd4hep::Detector& theDetector, st
 
 //It suffices to perform the calculations in the first quadrant due to the detector's symmetry.
 //The signs of the tracks' directions are ultimately determined by the momenta.
-//The rel_X0 refers to  mean interaction length traversing the material perpendicularly; must be <= 1 ! For jets draw is set to 0.
+//The rel_X0 refers to  mean interaction length traversing the material perpendicularly; must be <= 1 ! For drawing jets is set to 0.
 double calculateTrackLength(std::string barrelName, std::string endcapName, dd4hep::Detector& theDetector, double x, double y, double z, double px, double py, double pz, double rel_X0){
 
     CalorimeterDrawParams barrel = getCalorimeterParameters(theDetector, barrelName);
